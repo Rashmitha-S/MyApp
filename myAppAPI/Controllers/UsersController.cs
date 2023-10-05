152093cb85aa03d2eded1b8602b77217d075035d
Rashmitha S <rashmitha.s@championx.com>
2023-10-05 20:41:16 +0530

    first commit

---
+++ myAppAPI/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using myApp.Data;
+using myApp.Entities;
+using SQLitePCL;
+
+namespace myApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]  // /api/Users
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        [ActivatorUtilitiesConstructor]
+        [HttpGet]
+
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+            return users;
+        }
+        [ActivatorUtilitiesConstructor]
+        [HttpGet("{id}")]
+
+        public async  Task<ActionResult<AppUser>> getUsersById(int id)
+        {
+            return await _context.Users.FindAsync(id);
+
+        }
+
+    }
+}
---
+++ myAppAPI/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using myApp.Entities;
+
+namespace myApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
